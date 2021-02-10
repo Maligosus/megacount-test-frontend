7182e53c9efe1eb627abbedbbe654b4e2d963bf9
Maligosus <iceslaer@gmail.com>
2021-02-10 18:36:09 +0500

    init git

---
+++ src/pages/App/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+interface AppProps{
+
+};
+
+interface AppState{
+
+};
+
+class App extends React.Component<AppProps,AppState>{
+    constructor(props:AppProps){
+        super(props);
+    }
+    render():React.ReactNode{
+        return <div>
+            
+        </div>;
+    }
+}
